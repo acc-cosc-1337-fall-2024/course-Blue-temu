4c9e35791d7a22e7029ed36ca37aed6f5499ad85
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-02-10 20:18:32 +0000

    homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,31 @@
 ﻿//write include statements
+#include "hwexpressions.h"
+#include <iostream>
 
 
 //write namespace using statement for cout
 
 int main()
 {
-	
+	 double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+    std::cout << "Enter the meal amount: ";
+    std::cin >> meal_amount;
+
+    tax_amount = get_sales_tax_amount(meal_amount);
+
+    std::cout << "Enter the tip rate (in decimal): ";
+    std::cin >> tip_rate;
+
+    tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+    total = meal_amount + tax_amount + tip_amount;
+
+    std::cout << "Receipt:\n";
+    std::cout << "Meal Amount: $" << meal_amount << "\n";
+    std::cout << "Sales Tax: $" << tax_amount << "\n";
+    std::cout << "Tip Amount: $" << tip_amount << "\n";
+    std::cout << "Total: $" << total << "\n";
 
 	return 0;
 }
