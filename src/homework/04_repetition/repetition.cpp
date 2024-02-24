50cda3a42011e84ed124fd8c3b6d3e9eab17fb1d
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-02-24 18:45:54 +0000

    home work 4

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include "repetition.h"
+
 //add include statements
 
 //add function(s) code here
+int factorial(int num) {
+    int sum = 1;
+    while (num > 0) {
+        sum *= num;
+        num--;
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2) {
+    while (num1 != num2) {
+        if (num1 < num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        num1 -= num2;
+    }
+    return num1;
+}
