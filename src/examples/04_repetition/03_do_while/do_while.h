53d7e70d1b6fbbf1e9d07afc5724e9f01991836e
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-03-02 06:45:23 +0000

    do while

--- src/examples/04_repetition/03_do_while/do_while.h
+++ src/examples/04_repetition/03_do_while/do_while.h
@@ -1,4 +1,16 @@
+#include<iostream>
+#include<string>
+
+int sum_of_squares_do(int num);
+void display_numbers_do(int num);
+void display_str_do(std::string str);
+
+
 //Write void function protype named prompt_user with no parameters
+void prompt_user();
+
+void run_menu();
+void handle_menu_option(int num);
 
 
 
