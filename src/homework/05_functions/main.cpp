a00c63fbf182d6d778e6bdf660e2bf63911e4c45
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-03-03 05:31:54 +0000

    homework 5

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,40 @@
+#include <iostream>
+#include <string>
 
-int main() 
-{
+double string& dna
+std::string get_reverse_string(std::string str);
+std::string get_dna_complement(std::string dna);
 
-	return 0;
+int main() {
+    int choice;
+    std::string dna;
+
+    do {
+        std::cout << "Menu\n";
+        std::cout << "1- Get GC Content\n";
+        std::cout << "2- Get DNA Complement\n";
+        std::cout << "3- Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout << "Enter DNA string: ";
+                std::cin >> dna;
+                std::cout << "GC Content: " << get_gc_content(dna) << std::endl;
+                break;
+            case 2:
+                std::cout << "Enter DNA string: ";
+                std::cin >> dna;
+                std::cout << "DNA Complement: " << get_dna_complement(dna) << std::endl;
+                break;
+            case 3:
+                std::cout << "Exiting program...\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 3);
+
+    return 0;
 }
