2865238917dc2b08354a3e7f0c62bb1f8ead0023
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-03-30 19:37:31 +0000

    fix

--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -5,19 +5,3 @@
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
-
-TEST_CASE("Test get_gc_content") {
-    REQUIRE(get_gc_content(AGCTATAG) == .375);
-	REQUIRE(get_gc_content(CGCTATAG) == .50);
-}
-
-
-TEST_CASE("Test get_reverse_string") {
-    REQUIRE(get_reverse_string(GATATCGA) == .375);
-	REQUIRE(get_reverse_string(GATATCGC) == .50);
-}
-
-TEST_CASE("Test get_dna_complement") {
-     REQUIRE(get_dna_complement(AAAACCCGGT) == ACCGGGTTTT);
-	REQUIRE(get_dna_complement(CCCGGAAAAT) == ATTTTCCGGG);
-}
